e903e617ec6ce7b56fb085b996caf19425e240a9
Muhammad Yasser <mohyas364@gmail.com>
2018-11-19 04:32:11 +0200

    Add files via upload

---
+++ kong/js/plugin.js
@@ -0,0 +1,11 @@
+/*global google ,$*/
+
+$(function () {
+
+	"use strict";
+
+	$(".carousel, .carousel-item, .carousel-item img").height($(window).height()).width("100%");
+
+	$(".map").height($(window).height() - 150);
+
+});
